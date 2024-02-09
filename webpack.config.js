fc3c0d9e8192c1b84c3b16f055184fe8810d5a3e
Tõnu Tarve <tonu.tarve99@hotmail.com>
2024-02-09 10:50:46 +0200

    dev-server ja style/css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,18 @@ module.exports = {
     compress: true,
     port: 9000,
   },
-  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  
 };
