31187eeaa951364df7f6c55b5c57d7a99b30618a
Tõnu Tarve <tonu.tarve99@hotmail.com>
2024-02-09 08:56:18 +0200

    09.02 test

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let fs = require('fs');
-fs.writeFileSync('hello.txt', 'Hello Node!');
+let obj = {
+    name: "Paul",
+    hello() {
+        console.log("hello");
+    }
+}
+console.log(obj.name);
+obj.hello();
+
+let text = "Hello world";
+console.log(text.length);
+console.log(text.toUpperCase());
+let array = [1, 2, 3];
+array.push(4);
